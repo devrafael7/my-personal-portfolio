408cc145fb129de014784adde94e832dc266f087
Rafael <rafarafa99944@gmail.com>
2024-07-17 20:36:05 -0300

    animation added

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 document.getElementById('prev').addEventListener('click', function() {
     document.querySelector('.overflow-x-auto').scrollBy({
-        left: -500, // Ajuste conforme necessário
+        left: -500, 
         behavior: 'smooth'
     });
 });
 
 document.getElementById('next').addEventListener('click', function() {
     document.querySelector('.overflow-x-auto').scrollBy({
-        left: 500, // Ajuste conforme necessário
+        left: 500, 
         behavior: 'smooth'
     });
 });
 
-const lastOne = document.querySelector('.footerLastOne')
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting && !entry.target.classList.contains('animated')) {
+            entry.target.classList.add('show', 'animated');
+        }
+    });
+});
+
+const elements = document.querySelectorAll('.hideaway');
+elements.forEach((element) => observer.observe(element));
