e768427af0b5dfda7cf57a95c1b7b7b6a0edb5ef
Rafael <rafarafa99944@gmail.com>
2024-07-17 23:48:38 -0300

    transitions added in main card

--- script.js
+++ script.js
@@ -31,3 +31,43 @@ backToTopBtn.addEventListener('click', () => {
     });
 });
 
+document.addEventListener('DOMContentLoaded', () => {
+    const images = document.querySelectorAll('.mainPicture0, .mainPicture1, .mainPicture2');
+    let currentIndex = 0;
+
+    const showNextImage = () => {
+        images[currentIndex].classList.remove('visible');
+        images[currentIndex].classList.add('nVisible');
+
+        currentIndex = (currentIndex + 1) % images.length;
+
+        // Mostra a próxima imagem
+        images[currentIndex].classList.remove('nVisible');
+        images[currentIndex].classList.add('visible');
+    };
+
+    images[currentIndex].classList.add('visible');
+
+    setInterval(showNextImage, 5000);
+});
+
+
+const mainTitleName = document.getElementById('main-title-name');
+const text = mainTitleName.innerHTML;
+let titleNameIndex = 0;
+let isDeleting = false;
+let typingSpeed = 80; 
+function type() {
+    const currentText = text.substring(0, titleNameIndex);
+    mainTitleName.innerHTML = currentText;
+    if (!isDeleting && titleNameIndex === text.length) {
+        isDeleting = true;
+        typingSpeed = 50; 
+    } else if (isDeleting && titleNameIndex === 4) {
+        isDeleting = false;
+        typingSpeed = 100; 
+    }
+    titleNameIndex += isDeleting ? -1 : 1;
+    setTimeout(type, typingSpeed);
+}
+setTimeout(type, typingSpeed);
